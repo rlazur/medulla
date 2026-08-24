6834442be96288fd144e28f875700eb2a0f1ded3
Ryan LaZur <ryan.lazur.98@gmail.com>
2026-08-23 19:04:53 -0500

    update fiducial volume to include 10cm DENT cathode cut

--- selection/include/cuts.h
+++ selection/include/cuts.h
@@ -204,7 +204,7 @@ namespace cuts
     template<class T>
     bool fiducial_cut(const T & obj)
     {
-        return obj.is_fiducial && !(obj.vertex[0] > 210.215 && obj.vertex[1] > 60 && (obj.vertex[2] > 290 && obj.vertex[2] < 390));
+        return obj.is_fiducial && !(obj.vertex[0] > 210.215 && obj.vertex[1] > 60 && (obj.vertex[2] > 290 && obj.vertex[2] < 390)) && !(obj.vertex[0] > -5  && obj.vertex[0] < 5);
     }
     REGISTER_CUT_SCOPE(RegistrationScope::Both, fiducial_cut, fiducial_cut);
 
